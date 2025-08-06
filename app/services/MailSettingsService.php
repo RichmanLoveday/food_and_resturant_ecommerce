4196a5225f4b5d50e11e7ef14524b29e844e992d
RichmanLoveday <lovedayrichman@yahoo.com>
2025-08-06 16:36:17 +0100

    uncommented out code in mailsettings service

--- app/services/MailSettingsService.php
+++ app/services/MailSettingsService.php
@@ -9,23 +9,23 @@ class MailSettingsService
 {
     public function getSettings()
     {
-        // $mailSetting = Cache::rememberForever('mail_settings', function () {
-        //     $keys = [
-        //         'mail_driver',
-        //         'mail_host',
-        //         'mail_port',
-        //         'mail_username',
-        //         'mail_password',
-        //         'mail_encryption',
-        //         'mail_form_address',
-        //         'mail_receive_address',
-        //     ];
+        $mailSetting = Cache::rememberForever('mail_settings', function () {
+            $keys = [
+                'mail_driver',
+                'mail_host',
+                'mail_port',
+                'mail_username',
+                'mail_password',
+                'mail_encryption',
+                'mail_form_address',
+                'mail_receive_address',
+            ];
 
-        //     //? get the key and value of the specific keys found
-        //     return Setting::whereIn('key', $keys)->pluck('value', 'key')->toArray();
-        // });
+            //? get the key and value of the specific keys found
+            return Setting::whereIn('key', $keys)->pluck('value', 'key')->toArray();
+        });
 
-        return [];
+        return $mailSetting;
     }
 
 
