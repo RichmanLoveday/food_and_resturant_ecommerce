6989ea5cfafa92dd9b577c677675eb764963577d
RichmanLoveday <lovedayrichman@yahoo.com>
2025-08-07 12:16:15 +0100

    fixed file path issue

--- app/Providers/SettingsServiceProvider.php
+++ app/Providers/SettingsServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\services\SettingsService;
+use App\Services\SettingsService;
 use Illuminate\Support\ServiceProvider;
 
 class SettingsServiceProvider extends ServiceProvider
