8320b775d894cdb37e312b540bbf76ac3e0e004b
RichmanLoveday <lovedayrichman@yahoo.com>
2025-12-19 17:32:10 +0100

    fix: docker files

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
-echo "Running composer"
+echo "Running composer install..."
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Generating application key..."
-php artisan key:generate --show
+php artisan key:generate
 
-echo "Caching config..."
+echo "Caching config & routes..."
 php artisan config:cache
-
-echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
@@ -18,9 +17,14 @@ php artisan migrate --force
 echo "Creating storage symlink..."
 php artisan storage:link
 
-echo "Running npm"
+echo "Building frontend assets..."
 npm install
 npm run prod
+
+# Replace __PORT__ in Nginx config with Render dynamic $PORT
+echo "Setting dynamic port..."
+envsubst '$PORT' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default.tmp
+mv /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
 
 # Start PHP-FPM in background
 php-fpm &
